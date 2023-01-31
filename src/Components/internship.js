b3e9eeb3826ee9d7ce8a7be38fff31ee2a17e1ba
raj pandey <raj.pandey@contentstack.com>
2023-01-31 23:44:14 +0530

    added internship

--- src/Components/internship.js
+++ src/Components/internship.js
@@ -11,7 +11,50 @@ class InternshipComponent{
         head.appendChild(link);
     }
     render(){
+        const internshipDiv = document.createElement('div');
+        const internshipIntro = document.createElement('div');
+        const internshipIntroText = document.createElement('h2');
 
+        internshipDiv.classList.add("internship");
+        internshipIntro.id = "internship-intro";
+
+        internshipIntroText.innerText = Internship.title;
+        const htmlObject = [];
+        internshipDiv.appendChild(internshipIntro);
+        internshipIntro.appendChild(internshipIntroText);
+
+        for(let i = 0;i<Internship.internship.length;i++){
+            const internshipCard = document.createElement('div');
+            internshipCard.classList.add('internship-card');
+            this.menu.MenuFunction(internshipCard,'internship-card-company',[Internship.internship[i].company]);
+            this.menu.MenuFunction(internshipCard,'internship-card-title',[Internship.internship[i].title]);
+            const internshipskills = document.createElement('div');
+            internshipskills.classList.add('internship-card-skills');
+            this.menu.MenuFunction(internshipskills,Internship.class[4],Internship.internship[i].skills);
+            internshipCard.appendChild(internshipskills);
+            this.menu.MenuFunction(internshipCard,'internship-card-description',[Internship.internship[i].description]);
+            htmlObject.push(internshipCard);
+            // this.menu.MenuFunction(projectCard,'project-card-title',[Project.projects[i].title]);
+            // const projectSkills = document.createElement('div'); 
+            // projectSkills.classList.add("project-card-skills");
+            // this.menu.MenuFunction(projectSkills,Project.class[4],Project.projects[i].skills);
+            // projectCard.appendChild(projectSkills);
+            // this.menu.MenuFunction(projectCard,'project-card-description',[Project.projects[i].description]);
+            // htmlObject.push(projectCard);
+        }
+        console.log(htmlObject);
+        for(let i = 0;i<htmlObject.length;i=i+2){
+            const internshipCards = document.createElement('div');
+            internshipCards.classList.add('internship-cards');
+            const sliceArray = htmlObject.slice(i,i+2);
+            for(let j = 0;j<sliceArray.length;j++)
+            {
+                internshipCards.appendChild(sliceArray[j]);
+            }
+            internshipDiv.appendChild(internshipCards);
+
+        }
+        document.body.appendChild(internshipDiv);
     }
 }
 const ic = new InternshipComponent();
